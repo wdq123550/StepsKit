04578b81183aa3f7c115f5492f72a79807de5f52
Kim <wdq123550@qq.com>
2026-04-08 15:51:51 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StepsKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "StepsKit",
+            targets: ["StepsKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "StepsKit"
+        ),
+        .testTarget(
+            name: "StepsKitTests",
+            dependencies: ["StepsKit"]
+        ),
+    ]
+)
